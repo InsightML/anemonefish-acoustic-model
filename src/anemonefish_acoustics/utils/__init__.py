87b3c14c6d6e9a722501e313294f0576414e4cc0
Freddie <freddie@insightml.io>
2025-11-01 09:20:32 +0000

    refactor: utils

--- src/anemonefish_acoustics/utils/__init__.py
+++ src/anemonefish_acoustics/utils/__init__.py
@@ -3,40 +3,16 @@ Utility modules for the anemonefish acoustics package.
 """
 
 from .logger import get_logger, setup_basic_logging, get_default_logger
-from .config import (
-    load_config, 
-    save_config, 
-    merge_configs, 
-    create_default_binary_classifier_config,
-    write_default_config
-)
-from .visualization import (
-    plot_training_history,
-    plot_audio_waveform,
-    plot_spectrogram,
-    plot_feature_comparison,
-    plot_confusion_matrix,
-    plot_prediction_samples
-)
+from .config import load_config, save_config
 from .utils import pretty_path
 __all__ = [
     # Logger utilities
-    'setup_logger', 
-    'TrainingLogger',
+    'get_logger', 
+    'get_default_logger',
+    'setup_basic_logging',
     
     # Config utilities
     'load_config',
     'save_config',
-    'merge_configs',
-    'create_default_binary_classifier_config',
-    'write_default_config',
-    
-    # Visualization utilities
-    'plot_training_history',
-    'plot_audio_waveform',
-    'plot_spectrogram',
-    'plot_feature_comparison',
-    'plot_confusion_matrix',
-    'plot_prediction_samples'
     'pretty_path'
 ]
